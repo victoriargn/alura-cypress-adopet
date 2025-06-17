ebfb87765dfe40d48c40cbf8c8efe431c73c1cd6
victoriargn <vicregina.oliveira@gmail.com>
2025-06-16 23:09:27 -0300

    Adding new test

--- cypress/e2e/adopet/api/api-messages.cy.js
+++ cypress/e2e/adopet/api/api-messages.cy.js
@@ -3,6 +3,19 @@
 describe('API Adopet tests', () => {
   const authorization = `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJhOTIyZThlNi02NzY2LTQ4YjYtYmM3NS1lYzdlNjU1OWZiZGUiLCJhZG9wdGVyTmFtZSI6IkxhdXJhIFJlZ2luYSIsImlhdCI6MTc0OTY4Njc0NCwiZXhwIjoxNzQ5OTQ1OTQ0fQ.d5sB5bS9bzN0QeA-surLpR34-pWjb5PLhxLpZoJMvqY`;
 
+  it('Check if body contains the correct user name', () => {
+    cy.request({
+      method: 'GET',
+      url: 'https://adopet-api-i8qu.onrender.com/adotante/perfil/a922e8e6-6766-48b6-bc75-ec7e6559fbde',
+      headers: { authorization }
+    }).then((res) => {
+      expect(res.status).to.be.equal(200)
+      expect(res.body).is.not.empty
+      expect(res.body).to.have.property('perfil')
+      expect(res.body.perfil.nome).to.be.equal('Laura Regina')
+    })
+  });
+
   it('Check if messages sent by user are displayed successfully in the screen', () => {
     cy.request({
       method: 'GET',

1a8886bdf08b732dce659a6a3afdf37220162467
victoriargn <vicregina.oliveira@gmail.com>
2025-06-19 17:50:39 -0300

    Fixing api-messages-test

--- cypress/e2e/adopet/api/api-messages.cy.js
+++ cypress/e2e/adopet/api/api-messages.cy.js
@@ -1,12 +1,13 @@
 /// <reference types="cypress" />
 
 describe('API Adopet tests', () => {
+  const authorization = Cypress.env('authorization');
 
   it('Check if body contains the correct user name', () => {
     cy.request({
       method: 'GET',
-      url: 'https://adopet-api-i8qu.onrender.com/adotante/perfil/a922e8e6-6766-48b6-bc75-ec7e6559fbde',
-      headers: Cypress.env()
+      url: `https://adopet-api-i8qu.onrender.com/adotante/perfil/${Cypress.env('user_id')}`,
+      headers: { authorization }
 
     }).then((res) => {
       expect(res.status).to.be.equal(200)
@@ -17,10 +18,11 @@ describe('API Adopet tests', () => {
   });
 
   it('Check if messages sent by user are displayed successfully in the screen', () => {
+
     cy.request({
       method: 'GET',
-      url: 'https://adopet-api-i8qu.onrender.com/mensagem/a922e8e6-6766-48b6-bc75-ec7e6559fbde',
-      headers: Cypress.env()
+      url: `https://adopet-api-i8qu.onrender.com/mensagem/${Cypress.env('user_id')}`,
+      headers: { authorization }
     }).then((res) => {
       expect(res.status).to.be.equal(200)
       expect(res.body).is.not.empty
@@ -28,7 +30,7 @@ describe('API Adopet tests', () => {
     })
   });
 
-  it('Check if the 401 status code is returned by not sending authorization header', () => {
+  it.only('Check if the 401 status code is returned by not sending authorization header', () => {
     cy.request({
       method: 'GET',
       url: 'https://adopet-api-i8qu.onrender.com/mensagem/a922e8e6-6766-48b6-bc75-ec7e6559fbde',
